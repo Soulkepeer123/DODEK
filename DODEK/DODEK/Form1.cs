b9706fcfe014ac43279a2cc780a2721be3195f1b
Soulkepeer123 <risterio@yandex.ru>
2018-02-22 19:26:00 +0700

    NIHUJA NE RABOTAYET

--- DODEK/DODEK/Form1.cs
+++ DODEK/DODEK/Form1.cs
@@ -10,6 +10,36 @@ using System.Windows.Forms;
 
 namespace DODEK
 {
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Invalidate();
+        }
+        private void Form1_Load(object sender, PaintEventArgs e)
+        {
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Circle c = new Circle();
+            c.setXYC(1, 2, 3);
+            c.DoDraw(e);
+            Pen pen = new Pen(Color.Aqua);
+            e.Graphics.DrawEllipse(pen, 130, 130, 100, 100);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            Pen pen = new Pen(Color.Aqua);
+            e.Graphics.DrawEllipse(pen, 130, 130, 100, 100);
+        }
+    }
+
     class Figure
     {
         int x, y, c;
@@ -27,14 +57,7 @@ namespace DODEK
         public void DoDraw(PaintEventArgs e)
         {
             Pen pen = new Pen(Color.Aqua);
-            e.Graphics.DrawEllipse(pen, 130, 130, 10, 10);
-        }
-    }
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+            e.Graphics.DrawEllipse(pen, 130, 130, 100, 100);
         }
     }
 }
