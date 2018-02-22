231aa4297f3c2623179c49e7eebe3453ec5e9914
Soulkepeer123 <risterio@yandex.ru>
2018-02-22 18:52:46 +0700

    Circle

--- DODEK/DODEK/Form1.cs
+++ DODEK/DODEK/Form1.cs
@@ -9,11 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DODEK
-{   
+{
     class Figure
     {
         int x, y, c;
-        public void setXYC( int x, int y, int c)
+        public void setXYC(int x, int y, int c)
         {
             this.x = x;
             this.y = y;
@@ -21,6 +21,14 @@ namespace DODEK
         }
         public virtual void DoDraw(PaintEventArgs e) { }
 
+    }
+    class Circle : Figure
+    {
+        public void DoDraw(PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Aqua);
+            e.Graphics.DrawEllipse(pen, 130, 130, 10, 10);
+        }
     }
     public partial class Form1 : Form
     {
