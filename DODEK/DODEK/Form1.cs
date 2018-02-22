3ab96b9d3451ad9541e019b1149c32a1f5619417
Soulkepeer123 <risterio@yandex.ru>
2018-02-22 18:37:57 +0700

    1

---
+++ DODEK/DODEK/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DODEK
+{   
+    class Figure
+    {
+        int x, y, c;
+        public void setXYC( int x, int y, int c)
+        {
+            this.x = x;
+            this.y = y;
+            this.c = c;
+        }
+        public virtual void DoDraw(PaintEventArgs e) { }
+
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
